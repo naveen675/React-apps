cb0fd134f2db604d07d215ef5e9c2da59525e8c5
“naveen675” <spnaveen675@gmail.com>
2022-05-01 15:12:27 +0530

    Created Navigation

---
+++ router/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<h1>Home</h1>}></Route>
+        <Route path='about' element={<h1>About</h1>}></Route>
+        <Route path='contact' element={<h1>Contact</h1>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
